a0388d335b047ca78f52eb452ba944a5d0664f34
pzm0040 <patrickmcardell@gmail.com>
2023-01-12 21:28:06 -0500

    Added player score keeping in playRound function.  Initialized variables to 0

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// Constant Choices
+// constant choices
 
 const choices = ['rock', 'paper', 'scissors'];
+
+// initialize player and computer scores at 0
+
+let playerScore = 0;
+let computerScore = 0;
 
 // computer selection
 
@@ -10,11 +15,14 @@ function getComputerChoice() {
     return itemSelection;
 }
 
+// function to compare different RPS selections
 function playRound(playerSelection, computerSelction) {
     if (playerSelection == 'rock') {
         if (computerSelction == 'paper') {
+            computerScore ++;
             return 'You Lose, Paper beats Rock.';
         } else if (computerSelction == 'scissors') {
+            playerScore ++;
             return 'You win! Rock beats Scissors.';
         } else {
             return 'Draw... You both selected Rock.';
@@ -23,17 +31,21 @@ function playRound(playerSelection, computerSelction) {
         if (computerSelction == 'paper') {
             return 'Draw... You both selected Paper.';
         } else if (computerSelction == 'scissors') {
+            computerScore ++;
             return 'You lose, Scissors beats Paper.';
         } else {
+            playerScore ++;
             return 'You win! Paper beats Rock.';
         }
     } else if (playerSelection == 'scissors'){
         if (computerSelction == 'paper') {
+            playerScore ++;
             return 'You win! Scissors beats Paper.';
         } else if (computerSelction == 'rock') {
+            computerScore ++;
             return 'You lose, Rock beats scissors.';
         } else {
-            return 'Draw... You both selected scissors.'
+            return 'Draw... You both selected scissors.';
         }
     } else {
         return 'Invalid choice. Please select: rock, paper, or scissors.';
@@ -46,3 +58,4 @@ const playerSelectionFormatted = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelectionFormatted, computerSelection))
+console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
